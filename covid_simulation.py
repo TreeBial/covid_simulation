7de2743045e778cd93747780f0acc9f8b98124fc
TreeBial <kookies371@gmail.com>
2020-06-24 03:04:53 +0900

    Improve by adding some constants

--- covid_simulation.py
+++ covid_simulation.py
@@ -51,6 +51,11 @@ class Person:
         calculate distance between two people
         '''
         return (self.pos[0] - other.pos[0])**2 + (self.pos[1] - other.pos[1])**2 <= dist**2
+    def stop(self):
+        '''
+        stop the point
+        '''
+        self.vel = [0, 0]
     def change_state(self, to_what):
         '''
         change state
@@ -61,14 +66,27 @@ class Person:
 pygame.init()
 
 # Initialize Simulation Constants
+FPS = 60 # Frame Per Second
+
 screenW, screenH = 1440, 900
 screen = pygame.display.set_mode((screenW, screenH))
 
 mapL, mapT, mapSize = 100, 100, 500
 hosL, hosT, hosSize = 700, 300, 50
 
-FPS = 60
-CONT_RATE = 1
+mapColor = (16, 16, 16)
+hosColor = (16, 16, 16)
+
+CONTAGIOUS_RATE = 0.7 # probability of contagion if two people pass by each other for one second
+CONTAGIOUS_RATE_PER_FRAME = pow(CONTAGIOUS_RATE, 1/FPS)
+FATALITY_RATE = 0.9 # per one second
+FATALITY_RATE_PER_FRAME = pow(FATALITY_RATE, 1/FPS)
+CURE_RATE = 0.1 # per one second
+CURE_RATE_PER_FRAME = pow(CURE_RATE, 1/FPS)
+
+CONTAGIOUS_RADIUS = 15
+LEAST_INFECTED_SECONDS = 3
+
 V = 0.7 # velocity of points
 
 # Initialize Health States
@@ -102,17 +120,19 @@ while True:
         contagious = not infected.quarantined
         if contagious:
             for j, healthy in enumerate(HLTs):
-                if infected.close(healthy, 15) and random() <= CONT_RATE / FPS:
+                if infected.close(healthy, CONTAGIOUS_RADIUS) and \
+                    random() <= CONTAGIOUS_RATE_PER_FRAME:
                     healthy.change_state(INF)
                     INFs.append(HLTs.pop(j))
     # Calculate 2: Infected People probabistically Dead or Cured
     for i, infected in enumerate(INFs):
         infected.infect_time_count += 1
-        if infected.infect_time_count >= 3 * FPS:
-            if random() <= 0.01:
+        if infected.infect_time_count >= LEAST_INFECTED_SECONDS * FPS:
+            if random() <= FATALITY_RATE_PER_FRAME:
                 infected.change_state(DED)
                 DEDs.append(INFs.pop(i))
-            elif random() <= 0.1: # 0.01 + 0.09
+                infected.stop()
+            elif random() <= CURE_RATE_PER_FRAME + FATALITY_RATE_PER_FRAME: ### should be improved
                 infected.change_state(CUR)
                 CURs.append(INFs.pop(i))
     # Calculate 3: PCR scan and quarantine
@@ -128,14 +148,16 @@ while True:
     for person in people:
         person.move()
     # Draw 1: Map and Hopital
-    pygame.draw.rect(screen, (16, 16, 16), [100, 100, mapSize, mapSize]) # Map
-    pygame.draw.rect(screen, (255, 255, 255), [700, 300, 50, 50], 1) # Hopital
+    pygame.draw.rect(screen, mapColor, [mapL, mapT, mapSize, mapSize]) # Map
+    pygame.draw.rect(screen, hosColor, [hosL, hosT, hosSize, hosSize]) # Hopital
     # Draw 2: People
     for person in people:
         if person in QURs:
-            pygame.draw.circle(screen, person.state.color, list(map(lambda x, y: round(x/mapSize*hosSize + y), person.pos, [hosL, hosT])), 5)
+            repr_position = list(map(lambda x, y: round(x/mapSize*hosSize + y), person.pos, [hosL, hosT]))
+            pygame.draw.circle(screen, person.state.color, repr_position, 5)
         else:
-            pygame.draw.circle(screen, person.state.color, list(map(lambda x, y: round(x+y), person.pos, [mapL, mapT])), 5)
+            repr_position = list(map(lambda x, y: round(x+y), person.pos, [mapL, mapT]))
+            pygame.draw.circle(screen, person.state.color, repr_position, 5)
     pygame.display.flip()
 
 # quit
